437b8aa8563558dcc6c5d978fd580e1d2f66b405
Elronia <zar.catherine@gmail.com>
2020-10-03 00:53:53 -0400

    Done.

--- index.js
+++ index.js
@@ -9,31 +9,6 @@ let mondayWork = function(activity="go to the office") {
 
 let wrapAdjective = function(style="*") {
     return function(adjective="special") {
-      return `You are ${style}${adjective}${style}!`
+        return `You are ${style}${adjective}!`
     }
-}
-  
-const Calculator = {
-    add: function(a,b) {
-      return a + b;
-    },
-    subtract: function(a,b) {
-      return a - b
-    },
-    multiply: function(a,b) {
-      return a * b
-    },
-    divide: function(a,b) {
-      return a / b;
-    }
-}
-  
-let actionApplyer = function(start, ray) {
-let a = start
-  
-    for (let i = 0; i < ray.length; i++ ){
-      a = ray[i](a)
-    }
-  
-    return a
 }

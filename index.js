d344582a7e5c8971db7d7248bdf193b3b06fdfaa
Elronia <zar.catherine@gmail.com>
2020-10-03 01:04:41 -0400

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,32 @@ let mondayWork = function(activity="go to the office") {
 }
 
 let wrapAdjective = function(style="*") {
-    return function(adjective="special") {
-        return `You are ${style}${adjective}!`
+    return function(adjective="a hard worker") {
+        return `You are ${style}${adjective}${style}!`
     }
+}
+
+const Calculator = {
+    add: function(a,b) {
+      return a + b;
+    },
+    subtract: function(a,b) {
+      return a - b
+    },
+    multiply: function(a,b) {
+      return a * b
+    },
+    divide: function(a,b) {
+      return a / b;
+    }
+}
+
+let actionApplyer = function(startInt, arrayOfFunc) {
+    let a = startInt
+
+    for (let i = 0; i < arrayOfFunc.length; i++) {
+        a = arrayOfFunc[i](a)
+    }
+
+    return a
 }
